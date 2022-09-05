3e1d04cb13794f6b0e2d8bc44e44d585c2528e74
M-kirichenko <m.kirichenko.exceedteam@gmail.com>
2022-09-05 12:20:21 +0400

    added persistor

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,22 @@
 import { legacy_createStore as createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import { rootReducer } from "./root-reducer";
 
+const persistConfig = {
+  key: "root",
+  storage,
+  // whitelist: [],
+  // blacklist: [],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = createStore(
-  rootReducer,
+  persistedReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
-export default store;
+
+const persistor = persistStore(store);
+export { store, persistor };
